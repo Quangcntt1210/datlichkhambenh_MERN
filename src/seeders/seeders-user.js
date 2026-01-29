0ffa4cba0eea32ebd09c4b6594a8590243727b1a
Tquang <n.tien.quang1210@gmail.com>
2026-01-29 19:47:48 +0700

    create database

--- src/seeders/seeders-user.js
+++ src/seeders/seeders-user.js
@@ -1,12 +1,16 @@
 'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        email: 'admin@gmail.com',
+        password: 'admin123',
+        firstName: 'Quang',
+        lastName: 'Nguyen Tien',
+        address: 'Thanh Hoa',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
